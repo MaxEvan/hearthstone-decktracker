b5edd62859127c0ef9ac4e6eba5a58c3d8ad5bb7
ME9 <max.evangeliste@gmail.com>
2016-07-05 13:35:34 -0400

    Added Filters.jsx

--- app/components/Filters.jsx
+++ app/components/Filters.jsx
@@ -7,7 +7,23 @@ class Filters extends React.Component {
     }
 
     render() {
-        return <div>Filters</div>
+    	var costs = [0,1,2,3,4,5,6,7,8,9,10].map(item => <option>{item}</option>)
+        return 	<form>
+        			<div className="col-md-3">
+	        			<span>Name</span>
+        				<input className="form-control" style={{color: "black"}} type="text" onChange={this.handle}/>
+        			</div>
+        			<div className="col-md-1">
+	        			<span>Cost</span>
+	        			<select className="form-control" name="cost">
+	        				{costs}
+	        			</select>
+	        		</div>
+        			<div className="col-md-3">
+        				<br/>
+	        			<button className="btn btn-default">Search</button>
+	        		</div>
+        		</form>
     }
 }
 
