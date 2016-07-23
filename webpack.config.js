801d304917884be47e3f677b66eb8ddc5b788b7d
ME9 <max.evangeliste@gmail.com>
2016-07-23 10:04:30 -0400

    Reformated the webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,44 @@
 const merge = require('webpack-merge')
 const validate = require('webpack-validator')
 
-const PATHS = {
-	editDeckPage: './app/pages/editDeckPage.jsx',
-	public: 'public/'
-}
-
 const common = {
-	entry: {
-		editDeckPage: PATHS.editDeckPage
-	},
-	output: {
-		path: PATHS.public,
-		filename: '[name].js'
-	},
-	module: {
-		loaders: [
-			{ test: /\.scss$/, loader: 'style!css!sass' },
-			{ test: /\.css$/, loader: 'style!css' },
-			{ test: /(\.js$|\.jsx$)/, loader: 'babel', exclude: /(node_modules)/, query: {presets: ['es2015', 'react']}},
-			{ test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
-			{ test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' }
-		]
-	}
+    entry: {
+        editDeckPage: './app/pages/editDeckPage.jsx',
+        decksPage: './app/pages/decksPage.jsx'
+    },
+    output: {
+        path: 'public/',
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [
+            { test: /\.scss$/, loader: 'style!css!sass' },
+            { test: /\.css$/, loader: 'style!css' },
+            { test: /(\.js$|\.jsx$)/, loader: 'babel', exclude: /(node_modules)/, query: {presets: ['es2015', 'react']}},
+            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
+            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' }
+        ]
+    }
 }
 
 var config
 
 switch(process.env.npm_lifecycle_event) {
 case 'build':
-	config = merge(
-		common, 
-		{
-			devtool: 'source-map'
-		}
-	)
-	break
+    config = merge(
+        common, 
+        {
+            devtool: 'source-map'
+        }
+    )
+    break
 default:
-	config = merge(
-		common,
-		{
-			devtool: 'eval-source-map'
-		}
-	)
+    config = merge(
+        common,
+        {
+            devtool: 'eval-source-map'
+        }
+    )
 }
 
 module.exports = validate(config)
